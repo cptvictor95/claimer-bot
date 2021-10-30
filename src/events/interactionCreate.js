dc895f3da65def3befaa86a95f970d66565352b3
cptvictor95 <cpt.victor@hotmail.com>
2021-10-30 14:45:03 -0300

    upgrade interactionCreate event

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,8 +1,24 @@
 module.exports = {
   name: "interactionCreate",
-  execute(interaction) {
+  async execute(interaction) {
+    if (!interaction.isCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
     console.log(
-      `${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`
+      `${interaction.user.tag} in #${interaction.channel.name} triggered /${interaction.commandName}.`
     );
+
+    if (!command) return;
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
   },
 };
