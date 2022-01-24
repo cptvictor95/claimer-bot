38b94b43c261f37f4c9e5a72a88ba42b1bace76c
italofd <italoferreiradutra@gmail.com>
2022-01-24 16:59:39 -0300

    Creat first unitary test

---
+++ src/utils/verifyWritePlayer.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+module.exports = {
+  verifyWritePlayer: function (userId, queueString) {
+    const queue = JSON.parse(fs.readFileSync(queueString));
+    const allPlayersQueue = JSON.parse(
+      fs.readFileSync("./src/players-on-queue.json")
+    );
+    const verifyQueue = queue.find((player) => player.id === userId);
+    const verifyAllQueues = allPlayersQueue.find(
+      (player) => player.id === userId
+    );
+    if (queue.length > 0) {
+      if (verifyQueue === undefined) {
+        const verifyAllPlayersQueue = allPlayersQueue.find(
+          (player) => player.id === userId
+        );
+        if (verifyAllPlayersQueue === undefined) {
+          return "false";
+        }
+        return "partially";
+      }
+    }
+    if (queue.length === 0) {
+      if (verifyAllQueues !== undefined) {
+        return "partially";
+      }
+      return "false";
+    }
+    if (verifyAllQueues === undefined) {
+      if (verifyQueue !== undefined) {
+        return "partially";
+      }
+    }
+    return "true";
+  },
+};
