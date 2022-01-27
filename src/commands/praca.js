7574f63a0de9399b0c4a139d69648c6f91d30dca
italofd <italoferreiradutra@gmail.com>
2022-01-27 01:41:54 -0300

    Create rule for timeout in praca command

--- src/commands/praca.js
+++ src/commands/praca.js
@@ -476,6 +476,8 @@ module.exports = {
         //Verifica se o usuaria ainda esta na fila (pois pode ja ter usado um leave)
         console.log(`${user.id} entrou`);
 
+        const timeoutDate = Date.now();
+
         let allPlayersQueueTimeout = JSON.parse(
           fs.readFileSync("./src/players-on-queue.json")
         );
@@ -492,6 +494,7 @@ module.exports = {
         let timeoutQueue = JSON.parse(
           fs.readFileSync(`./src/magic-square/${floor}/${formattedChamber}`)
         );
+
         console.log(`${user.id} timeoutQueue`, timeoutQueue);
 
         if (!check) {
@@ -503,6 +506,12 @@ module.exports = {
         }
 
         if (timeoutQueue[0].id !== user.id) {
+          return;
+        }
+
+        //Preciso impedir que o usuario seja removido antes da fila, vendo o endsAt e comparando a data Atual
+        //13:20 ----> tocka as 13:00
+        if (timeoutDate < timeoutQueue[0].endsAt) {
           return;
         }
         //-----//
